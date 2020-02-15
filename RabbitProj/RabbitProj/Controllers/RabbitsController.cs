994bd239fc04cd71aef31d9f2fa3fe7695c93358
Stefan Fanaru <fanaru.stefan@gmail.com>
2020-02-15 11:56:29 +0200

    Commit 1

---
+++ RabbitProj/RabbitProj/Controllers/RabbitsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitProj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RabbitsController : ControllerBase
+    {
+        // GET: api/Rabbits
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Rabbits/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Rabbits
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Rabbits/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RabbitProj/RabbitProj/Models/Rabbit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitProj.Models
+{
+    public class Rabbit
+    {
+        public enum FurColors
+        {
+            White,
+            Brown,
+            Black,
+            Grey
+        }
+        public enum EyeColors
+        {
+            Blue,
+            Black,
+            Red
+        }
+        public enum Genders
+        {
+            Male,
+            Female
+        }
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public FurColors FurColor { get; set; }
+
+        [Required]
+        public EyeColors EyeColor { get; set; }
+
+        [Required]
+        public Genders Gender { get; set; }
+
+
+    }
+}
